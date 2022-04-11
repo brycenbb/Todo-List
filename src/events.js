a470c9825299eda5d7bea093be6d3fe6c964c8a9
brycen <brycenbb1996@gmail.com>
2022-04-11 14:29:01 +0100

    setting up todoList items

--- src/events.js
+++ src/events.js
@@ -1,5 +1,13 @@
+import todoItem from "./todoList";
+
 let eventSetup = () => {
-    console.log("hi");
+    // let projectDiv = document.querySelector('.projectList');
+    // projectDiv.childNodes.forEach(node => {
+    //     node.addEventListener('click', display)
+    // });
+    console.log('to be completed');
+    let test = todoItem("This is a test item", "not implemented yet","Dec 12","high",true);
+    test.display();
 }
 
 export default eventSetup;
--- src/todoList.js
+++ src/todoList.js
@@ -1,5 +1,39 @@
 const todoItem = (title, description, dueDate, priority, completionStatus) => {
+    console.log("gettging threre");
+    const display = () => {
+        const container = document.querySelector('.container');
+        let box = document.createElement('div');
+        box.classList.add('listItem');
+        if(priority === "high"){
+            box.style.borderLeft = "2px solid red";
+        }
+        else if(priority === "medium") {
+            box.style.borderLeft = "2px solid yellow";
+        }
+        else {
+            box.style.borderLeft = "2px solid green";
+        }
+        let checkBox = document.createElement('div');
+        //Need to add a SVG to checkbox for its 2 states//
+        if(completionStatus) {
+            checkBox.style
+        }
+        else {
 
+        }
+        box.appendChild(checkBox);
+        let text = document.createElement('div');
+        text.textContent = title;
+        text.style.paddingRight = "350px";
+        box.appendChild(text);
+        let date = document.createElement('div');
+        date.textContent = dueDate;
+        date.style.color = "#BB9BB0";
+        box.appendChild(date);
+        container.appendChild(box);
+        return box;
+    }
+    return {display};
 }
 
 const todoList = () => {
@@ -12,3 +46,5 @@ const todoList = () => {
 
     return {addItem};
 }
+
+export default todoItem;
