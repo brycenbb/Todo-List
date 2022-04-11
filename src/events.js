2724416befb5141bba1d8b6fc1a4939b8bdc8f37
brycen <brycenbb1996@gmail.com>
2022-04-11 17:06:55 +0100

    follow suit

--- src/events.js
+++ src/events.js
@@ -6,8 +6,11 @@ let eventSetup = () => {
     //     node.addEventListener('click', display)
     // });
     console.log('to be completed');
-    let test = todoItem("This is a test item", "not implemented yet","Dec 12","high",true);
+    let test = todoItem("This is a test item", "not implemented yet","Dec 12","high",false);
+    let test2 = todoItem("This is a test item", "not implemented yet","Dec 12","high",true);
+
     test.display();
+    test2.display();
 }
 
 export default eventSetup;
